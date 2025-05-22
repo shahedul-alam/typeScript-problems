6d406a31eaa0192e2f13b7ec69c5155c27c83d59
shahedul-alam <shaheedalamcontact@gmail.com>
2025-05-23 00:08:09 +0600

    created a class with private properties and extended it into another class then used that class to get the parents and own data

--- problems.ts
+++ problems.ts
@@ -18,6 +18,32 @@ function filterByRating(
 
 function concatenateArrays<T>(...arrays: T[][]): T[] {
   return arrays.flat();
-};
+}
 
+class Vehicle {
+  private make: string;
+  private year: number;
+
+  constructor(make: string, year: number) {
+    this.make = make;
+    this.year = year;
+  }
+
+  getInfo() {
+    return { Make: this.make, Year: this.year };
+  }
+}
+
+class Car extends Vehicle {
+  private model: string;
+
+  constructor(make: string, year: number, model: string) {
+    super(make, year);
+    this.model = model;
+  }
+
+  getModel() {
+    return { Model: this.model };
+  }
+}
 
