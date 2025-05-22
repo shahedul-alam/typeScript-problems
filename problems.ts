73cbd7d4d90598f6cf90d34e9f11da401d8dbec7
shahedul-alam <shaheedalamcontact@gmail.com>
2025-05-23 02:29:20 +0600

    created the squareAsync function

--- problems.ts
+++ problems.ts
@@ -88,3 +88,14 @@ function getDayType(day: Day): string {
   return "Weekdays";
 }
 
+async function squareAsync(n: number): Promise<number> {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if(n < 0) {
+        reject(new Error("Negative number not allowed"));
+      } else {
+        resolve(n * n);
+      }
+    }, 1000);
+  })
+}
