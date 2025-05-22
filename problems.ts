1166703a0467613077eb29f783bcab46f9a97868
shahedul-alam <shaheedalamcontact@gmail.com>
2025-05-22 23:09:21 +0600

    created a filterBYRating function that filters an array of objects by the rating property, returing only items with a rating of 4 or more

--- problems.ts
+++ problems.ts
@@ -1,9 +1,17 @@
 function formatString(input: string, toUpper?: boolean): string {
-  if(typeof toUpper === "undefined" || toUpper) {
+  if (typeof toUpper === "undefined" || toUpper) {
     return input.toUpperCase();
   }
 
   return input.toLowerCase();
 }
 
+function filterByRating(
+  items: { title: string; rating: number }[]
+): { title: string; rating: number }[] {
+  const filteredBooks: { title: string; rating: number }[] = items.filter(
+    (item: { title: string; rating: number }) => item.rating >= 4
+  );
 
+  return filteredBooks;
+}
